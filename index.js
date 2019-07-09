7792064a8b7f6b06d6228e7149f1ca120d64fba6
ecrmnn <danieleckermann@gmail.com>
2019-07-09 12:29:37 +0200

    Adds Elixir _build

--- index.js
+++ index.js
@@ -6,16 +6,11 @@ const handle = async (event, context, callback) => {
   // const eventString = `~s(${JSON.stringify(event)})`;
   // const contextString = `~s(${JSON.stringify(context)})`;
 
-  const mixPath = '../bin/elixir/1.8.0/bin/mix';
+  const binary = './src/_build/dev/rel/lambda/bin/lambda_rc_exec.sh';
 
   try {
-    const stdout = execSync([
-      'cd src/',
-      `${mixPath} run -e 'Lambda.handle()'`,
-    ].join(' && '),
-    {
+    const stdout = execSync(`${binary} eval 'Lambda.handle'`, {
       encoding: 'utf8',
-      stdio: 'inherit',
     });
 
     const output = stdout.split('\n');
@@ -26,14 +21,12 @@ const handle = async (event, context, callback) => {
 
     callback(null, {
       statusCode: 200,
-      // body: output.toString(),
-      body: 'ok',
+      body: output.toString(),
     });
   } catch (error) {
     callback(null, {
       statusCode: 200,
-      // body: JSON.stringify(error),
-      body: 'error',
+      body: JSON.stringify(error),
     });
   }
 };
---
+++ src/_build/prod/lib/lambda/ebin/lambda.app
@@ -0,0 +1,6 @@
+{application,lambda,
+             [{applications,[kernel,stdlib,elixir,logger,jason,distillery]},
+              {description,"lambda"},
+              {modules,['Elixir.Lambda']},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
